1ed6b83b312dd0ea4ba9a328c30e8ca79270705f
n-abrij <omollojibran@gmail.com>
2026-07-30 15:58:50 +0300

    feat: Header Component

--- src/app/layout/authenticated-layout/authenticated-layout.ts
+++ src/app/layout/authenticated-layout/authenticated-layout.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../../component/header/header.component';
 
 @Component({
   selector: 'app-authenticated-layout',
-  imports: [],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './authenticated-layout.html',
   styleUrl: './authenticated-layout.css',
 })
